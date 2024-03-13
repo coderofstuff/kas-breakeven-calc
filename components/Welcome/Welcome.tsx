86626e6cdc3a4fae6f7930df66c495c06bf3de5f
coderofstuff <114628839+coderofstuff@users.noreply.github.com>
2024-03-13 23:56:49 +0000

    Show total earnings until loss or lifespan

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import { NumberInput, Table, TextInput, Button, Group, Text, Box, Stack, Anchor, Grid } from '@mantine/core';
+import { NumberInput, Table, TextInput, Button, Group, Text, Box, Stack, Anchor, Grid, Divider } from '@mantine/core';
 import { DateInput } from '@mantine/dates';
 import { useForm } from '@mantine/form';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import QRCode from 'react-qr-code';
 
-
-function calcBreakeven({ machineCost, machineHashrate, wattage, costPerKwh, hashRateIncreasePerDay, startingHashrate, startingDate, price, dailyPriceIncrease }) {
+function calcBreakeven({ machineCost, machineHashrate, machineLifespan, wattage, costPerKwh, hashRateIncreasePerDay, startingHashrate, startingDate, price, dailyPriceIncrease }) {
   let netEarnings = 0;
+  let totalKasEarnings = 0;
   let hashrate = Number(startingHashrate);
   let i = 0;
   let currentDate = new Date(startingDate);
   let chromaticReward = 220;
   let chromaticDate = new Date('2023-05-08');
+  let machineLifespanEndDate = new Date(currentDate);
+  machineLifespanEndDate.setFullYear(machineLifespanEndDate.getFullYear() + machineLifespan);
 
   let checkDate = new Date(chromaticDate);
   checkDate.setDate(chromaticDate.getDate() + 30);
@@ -27,7 +29,9 @@ function calcBreakeven({ machineCost, machineHashrate, wattage, costPerKwh, hash
     chromaticReward = Number((chromaticReward * (0.5 ** (1/12))).toFixed(8));
   }
 
-  while (netEarnings < machineCost) {
+  let breakEvenDate = null;
+
+  while (currentDate <= machineLifespanEndDate) {
       while (currentDate > checkDate) {
         chromaticDate = checkDate;
         checkDate.setDate(chromaticDate.getDate() + 30);
@@ -36,27 +40,42 @@ function calcBreakeven({ machineCost, machineHashrate, wattage, costPerKwh, hash
 
       const blockReward = chromaticReward;
       const machineHashrateTH = machineHashrate / 1000;
-      const totalRewards = price * blockReward * 86400 * machineHashrateTH / hashrate;
+      const currentRewardInKas = blockReward * 86400 * machineHashrateTH / hashrate;
+      const totalRewards = price * currentRewardInKas;
       const totalCost = wattage * 24 * costPerKwh / 1000;
 
       const currentNetEarning = (totalRewards - totalCost);
 
       if (currentNetEarning <= 0) {
+        if (!breakEvenDate) {
+          breakEvenDate = new Date(currentDate);
+        } else {
+          console.info('About to go into the negative but breakeven date found', breakEvenDate);
+        }
         console.info(hashrate, blockReward, totalRewards, totalCost, "About to take on a loss. Cannot mine any further");
-        return {dailyEarnings, breakEvenDate: currentDate, netEarnings, canBreakEven: false};
+        return {dailyEarnings, breakEvenDate, totalKasEarnings, netEarnings, canBreakEven: netEarnings >= machineCost, lifespanDate: currentDate, isLifeTimeReached: false};
       }
 
       netEarnings += currentNetEarning;
+      totalKasEarnings += currentRewardInKas;
+
+      if (!breakEvenDate && netEarnings >= machineCost) {
+        breakEvenDate = new Date(currentDate);
+        console.info('---------------------');
+        console.info('Found the breakeven date', breakEvenDate);
+        console.info('---------------------');
+      }
 
       dailyEarnings.push({
         amountInFiat: currentNetEarning.toFixed(2),
-        amountInKas: (currentNetEarning/price).toFixed(2),
+        amountInKas: currentRewardInKas.toFixed(2),
         totalEarningsInFiat: netEarnings.toFixed(2),
-        totalEarningsInKas: (netEarnings/price).toFixed(2),
+        totalEarningsInKas: totalKasEarnings.toFixed(2),
         networkHashrate: hashrate,
         date: new Date(currentDate),
         price,
       });
+
       console.info(currentDate, `Net Earnings USD => KAS: ${netEarnings.toFixed(2)} => ${(netEarnings/price).toFixed(2)}\tDaily USD => KAS: ${totalRewards.toFixed(3)} => ${(totalRewards / price).toFixed(2)}\tNetwork Hashrate: ${(hashrate / 1000).toFixed(2)}\tPH/s`);
 
       // Iterate
@@ -69,14 +88,22 @@ function calcBreakeven({ machineCost, machineHashrate, wattage, costPerKwh, hash
   // Bring it back one day
   currentDate.setDate(currentDate.getDate() - 1);
 
-  return {dailyEarnings, breakEvenDate: currentDate, netEarnings, canBreakEven: true};
+  if (!breakEvenDate) {
+    breakEvenDate = new Date(currentDate);
+  }
+
+  return {dailyEarnings, breakEvenDate, totalKasEarnings, netEarnings, canBreakEven: true, lifespanDate: machineLifespanEndDate, isLifeTimeReached: true};
 }
 
 export function Welcome() {
   const [dailyEarnings, setDailyEarnings] = useState([]);
   const [canBreakEven, setCanBreakEven] = useState();
+  const [isLifeTimeReached, setIsLifeTimeReached] = useState();
   const [hasCalculation, setHasCalculation] = useState(false);
   const [breakEvenDate, setBreakEvenDate] = useState();
+  const [lifespanDate, setLifespanDate] = useState();
+  const [netEarnings, setNetEarnings] = useState();
+  const [totalKasReward, setTotalKasReward] = useState();
 
   const today = new Date();
 
@@ -84,6 +111,7 @@ export function Welcome() {
     initialValues: {
       machineCost: 750,
       machineHashrate: 200,
+      machineLifespan: 5,
       wattage: 100,
       costPerKwh: 0.1,
       hashRateIncreasePerDay: 100,
@@ -127,18 +155,23 @@ export function Welcome() {
   }, []);
 
   function handleSubmit(values) {
-    const {dailyEarnings, breakEvenDate, netEarnings, canBreakEven} = calcBreakeven(values);
+    const {dailyEarnings, breakEvenDate, totalKasEarnings, netEarnings, canBreakEven, lifespanDate, isLifeTimeReached} = calcBreakeven(values);
 
-    console.info(dailyEarnings, breakEvenDate, netEarnings, canBreakEven);
+    console.info(dailyEarnings, breakEvenDate, totalKasEarnings, netEarnings, canBreakEven, lifespanDate, isLifeTimeReached);
 
     setBreakEvenDate(breakEvenDate);
     setDailyEarnings(dailyEarnings);
     setCanBreakEven(canBreakEven);
+    setIsLifeTimeReached(isLifeTimeReached);
     setHasCalculation(true);
+    setLifespanDate(lifespanDate);
+    setNetEarnings(netEarnings);
+    setTotalKasReward(totalKasEarnings);
   }
 
   let table = null;
   let breakEvenText = null;
+  let lifeTimeEarnings = null;
 
   if (hasCalculation) {
     const dateText = breakEvenDate.toISOString().split('T')[0]
@@ -149,6 +182,31 @@ export function Welcome() {
         </Text>
       </Group>
     );
+
+    if (canBreakEven) {
+      const lifespanDateText = lifespanDate.toISOString().split('T')[0]
+      let lifespanText = null;
+      if (isLifeTimeReached) {
+        lifespanText = 'Until end of machine lifespan on: ';
+      } else {
+        lifespanText = 'Until mining at a loss on: ';
+      }
+
+      lifespanText += lifespanDateText;
+      
+      lifeTimeEarnings = (
+        <Group position='center' m={'1rem'}>
+          <Stack >
+            <Text fw={600}>
+              Total earnings: {totalKasReward.toFixed(2)} KAS and ${netEarnings.toFixed(2)}
+            </Text>
+            <Text fw={600}>
+              {lifespanText}
+            </Text>
+          </Stack>
+        </Group>
+      );
+    }
 
     const rows = dailyEarnings.map(({date, amountInFiat, amountInKas, totalEarningsInFiat, totalEarningsInKas, networkHashrate, price }) => {
       return (
@@ -184,23 +242,42 @@ export function Welcome() {
   const DONATION_ADDR = 'kaspa:qpfp8gzttv5c356p8cdj0kv56py4n29w97prxrav65m0zpn58yuaz3awck5vl';
   return (
     <>
-    <Box maw={340} mx="auto">
+    <Box maw={390} mx="auto">
       <form onSubmit={form.onSubmit(handleSubmit)}>
-        <NumberInput
-          withAsterisk
-          label="Machine Cost"
-          placeholder="1000"
-          {...form.getInputProps('machineCost')}
-        />
 
-        <NumberInput
-          withAsterisk
-          label="Machine Hashrate (GH/s)"
-          placeholder="1000"
-          step={1}
-          min={0}
-          {...form.getInputProps('machineHashrate')}
-        />
+        <Text align='center' fw={600}>Machine Details</Text>
+        <Grid>
+          <Grid.Col span={4}>
+            <NumberInput
+              withAsterisk
+              label="Cost"
+              placeholder="1000"
+              {...form.getInputProps('machineCost')}
+            />
+          </Grid.Col>
+
+          <Grid.Col span={4}>
+            <NumberInput
+              withAsterisk
+              label="Hashrate (GH/s)"
+              placeholder="1000"
+              step={1}
+              min={0}
+              {...form.getInputProps('machineHashrate')}
+            />
+          </Grid.Col>
+          <Grid.Col span={4}>
+            <NumberInput
+                withAsterisk
+                label="Lifespan (Years)"
+                placeholder="1000"
+                step={1}
+                max={5}
+                min={1}
+                {...form.getInputProps('machineLifespan')}
+              />
+          </Grid.Col>
+        </Grid>
 
         <Grid>
           <Grid.Col span={6}>
@@ -222,19 +299,28 @@ export function Welcome() {
           </Grid.Col>
         </Grid>
 
-        <NumberInput
-          withAsterisk
-          label="Hashrate Increase per Day (TH/s)"
-          placeholder="147"
-          {...form.getInputProps('hashRateIncreasePerDay')}
-        />
+        <Divider m={'1rem'}/>
 
-        <NumberInput
-          withAsterisk
-          label="Network Hashrate on start of mining (TH/s)"
-          placeholder="4730"
-          {...form.getInputProps('startingHashrate')}
-        />
+        <Text align='center' fw={600}>Network and Other Details</Text>
+
+        <Grid>
+          <Grid.Col span={6}>
+            <NumberInput
+              withAsterisk
+              label="Network Hashrate on start of mining (TH/s)"
+              placeholder="4730"
+              {...form.getInputProps('startingHashrate')}
+            />
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <NumberInput
+              withAsterisk
+              label="Daily Hashrate Increase (TH/s)"
+              placeholder="147"
+              {...form.getInputProps('hashRateIncreasePerDay')}
+            />
+          </Grid.Col>
+        </Grid>
 
         <Grid>
           <Grid.Col span={6}>
@@ -278,7 +364,6 @@ export function Welcome() {
 
     <Group position='center' mt={'1rem'}>
         <Stack align='center' style={{gap: 0}} justify='end'>
-          
 
           <div className="DonationQR">
             <QRCode style={{'width': '100%', 'height': '100%'}}
@@ -299,6 +384,8 @@ export function Welcome() {
       </Group>
 
     {breakEvenText}
+
+    {lifeTimeEarnings}
 
     {table}
     </>
